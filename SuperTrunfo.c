af1404226e54795621d5d640b47a784bc3b95d75
Christian777-z <miccoozzii@gmail.com>
2025-03-07 22:36:03 -0300

    Create SuperTrunfo.c

---
+++ SuperTrunfo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+  char estado[20];
+  char codigo[5];
+  char cidade[20];
+  int populacao;
+  float area;
+  float pib;
+  int atrativos;
+
+  printf ("Carta 1\n");
+  printf ("Estado: São Paulo\n");
+  printf ("Código: A01\n");
+  printf ("Cidade: Taboão da Serra\n");
+  printf ("População: 273542\n");
+  printf ("Área: 20,48 km^2\n");
+  printf ("PIB: 10.132.458.254,38\n");
+  printf ("Atrativos: 18\n");
+
+
+  printf ("Carta 2\n");
+  printf ("Estado: Rio de Janeiro\n");
+  printf ("Código: A02\n");
+  printf ("Cidade: Angra dos Reis\n");
+  printf ("População: 167418\n");
+  printf ("Área: 816,3 km^2\n");
+  printf ("PIB: 2.642.047.486,15\n");
+  printf ("Atrativos: 26\n");
+
+}
